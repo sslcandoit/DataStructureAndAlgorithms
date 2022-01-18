ce42733827d76b5bfb507532cebc4e5aee080027
sslee <91924792+sslcandoit@users.noreply.github.com>
2022-01-18 10:32:04 -0500

    Time: 4 ms (20.28%), Space: 43.3 MB (19.74%) - LeetHub

---
+++ 95-unique-binary-search-trees-ii/95-unique-binary-search-trees-ii.java
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<TreeNode> generateTrees(int n) {
+        if(n==0) return new ArrayList<>();
+        return helper(1, n);
+    }
+    private List<TreeNode> helper(int start, int end){
+        
+        List<TreeNode> list=new ArrayList<>();
+        if(start>end){
+            list.add(null);
+            return list;
+        } 
+        for(int i=start; i<=end; i++){
+            List<TreeNode> left=helper(start, i-1);
+            List<TreeNode> right=helper(i+1, end);
+        
+            for(TreeNode l:left){
+                for(TreeNode r:right){
+                    TreeNode newNode=new TreeNode(i);
+                    newNode.left=l;
+                    newNode.right=r;
+                    list.add(newNode);
+                }
+            }
+        }
+       return list; 
+    }
+}
