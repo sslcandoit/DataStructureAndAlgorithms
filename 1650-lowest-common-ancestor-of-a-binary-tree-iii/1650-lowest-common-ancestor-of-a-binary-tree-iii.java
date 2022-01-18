59052ee4431d3fcef7f6d7d3ab8685422a085a04
sslee <91924792+sslcandoit@users.noreply.github.com>
2022-01-18 09:33:04 -0500

    Time: 35 ms (40.20%), Space: 44.2 MB (35.95%) - LeetHub

--- 1650-lowest-common-ancestor-of-a-binary-tree-iii/1650-lowest-common-ancestor-of-a-binary-tree-iii.java
+++ 1650-lowest-common-ancestor-of-a-binary-tree-iii/1650-lowest-common-ancestor-of-a-binary-tree-iii.java
@@ -10,6 +10,19 @@ class Node {
 
 class Solution {
     public Node lowestCommonAncestor(Node p, Node q) {
+        Node n1=p;
+        Node n2=q;
+        while(n1 !=n2){
+            n1=n1.parent;
+            n2=n2.parent;
+            if(n1==null) n1=q;
+            if(n2==null) n2=p;
+        }
+        return n1;
+    }
+    
+    /*
+    public Node lowestCommonAncestor(Node p, Node q) {
         Set<Node> set=new HashSet<>();
         while(p!=null){
             set.add(p);
@@ -17,5 +30,5 @@ class Solution {
         }
         while(!set.contains(q)) q=q.parent;
         return q;
-    }
+    }      */
 }
