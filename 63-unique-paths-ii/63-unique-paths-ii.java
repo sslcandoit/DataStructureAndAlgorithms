52540a819d3b04331bfc2c9e921754d72f2480ce
sslee <91924792+sslcandoit@users.noreply.github.com>
2022-01-26 04:07:34 -0500

    Time: 1 ms (45.47%), Space: 38.7 MB (47.87%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.java
+++ 63-unique-paths-ii/63-unique-paths-ii.java
@@ -1,5 +1,6 @@
 class Solution {
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        /*
         int m=obstacleGrid.length;
         int n=obstacleGrid[0].length;
         int[] dp=new int[n];
@@ -11,6 +12,18 @@ class Solution {
             }
         }
         return dp[n-1];
+        */
+        int m=obstacleGrid.length;
+        int n=obstacleGrid[0].length;
+        int[] dp=new int[m];
+        dp[0]=1;
+        for(int j=0; j<n; j++){
+            for(int i=0; i<m; i++){
+                if(obstacleGrid[i][j]==1) dp[i]=0;
+                else if(i>0) dp[i]+=dp[i-1];
+            }
+        }
+        return dp[m-1];
     }
     /*
     length=m
