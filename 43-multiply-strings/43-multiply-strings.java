53600a854f60f5ed3a98ffaaa38cfe950a3b0230
sslee <91924792+sslcandoit@users.noreply.github.com>
2022-01-27 10:02:53 -0500

    Time: 11 ms (39.99%), Space: 43.6 MB (11.38%) - LeetHub

---
+++ 43-multiply-strings/43-multiply-strings.java
@@ -0,0 +1,29 @@
+class Solution {
+    public String multiply(String num1, String num2) {
+        int len1=num1.length();
+        int len2=num2.length();
+        
+        int[] product=new int[len1+len2];//999*999=998001, so the maxLen is len1+len2
+        
+        int res=0;
+        for(int i1=len1-1; i1>=0; i1--){
+            int digit1=num1.charAt(i1)-'0';
+            for(int i2=len2-1; i2>=0; i2--){
+                int digit2=num2.charAt(i2)-'0';
+                
+                int posPre=i1+i2;
+                int posPos=i1+i2+1;
+                
+                int cur=digit1*digit2+product[posPre]*10+product[posPos];
+                product[posPre]=cur/10;
+                product[posPos]=cur%10;
+            }
+        }
+        StringBuilder sb=new StringBuilder();
+        for(int n:product){
+            if(sb.length()==0 && n==0) continue;
+            sb.append(n+"");
+        }
+        return sb.length()==0? "0": sb.toString();
+    }
+}
