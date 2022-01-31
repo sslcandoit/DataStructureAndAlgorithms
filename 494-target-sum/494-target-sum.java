3edea47ff29490fb4a66f7e07074452186d5c294
sslee <91924792+sslcandoit@users.noreply.github.com>
2022-01-30 23:57:40 -0500

    Time: 7 ms (88.35%), Space: 42.2 MB (25.75%) - LeetHub

--- 494-target-sum/494-target-sum.java
+++ 494-target-sum/494-target-sum.java
@@ -1,5 +1,22 @@
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
+        int total=Arrays.stream(nums).sum();
+        int[] dp=new int[total*2+1];
+        dp[total+nums[0]]=1;
+        dp[total-nums[0]]+=1;
+        
+        for(int i=1; i<nums.length; i++){
+            int[] temp=new int[total*2+1];
+            for(int sum=-total; sum<=total; sum++){
+                if(dp[sum+total]>0){
+                    temp[sum+total+nums[i]]+=dp[sum+total];
+                    temp[sum+total-nums[i]]+=dp[sum+total];
+                }
+            }
+            dp=temp;
+        }
+        return Math.abs(target)>total? 0:dp[target+total];
+        /*
         Map<Integer, Integer> dp=new HashMap<>();
         dp.put(0,1);
         for(int num:nums){
@@ -13,5 +30,6 @@ class Solution {
             dp=temp;
         }
         return dp.getOrDefault(target,0);
+        */
     }
 }
