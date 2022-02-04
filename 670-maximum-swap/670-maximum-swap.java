239aa7b7bc96481271a27eff85f9f609f7fc85e2
sslee <91924792+sslcandoit@users.noreply.github.com>
2022-02-04 18:16:40 -0500

    Time: 1 ms (63.87%), Space: 41.1 MB (5.02%) - LeetHub

---
+++ 670-maximum-swap/670-maximum-swap.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int maximumSwap(int num) {
+        char[] ch=(num+"").toCharArray();
+        
+        for(int i=0; i<ch.length; i++){
+            int max_index=i;
+            int count=0;
+            for(int j=i+1; j<ch.length; j++){
+                if(ch[j]-'0'==ch[i]-'0') continue;
+                if((ch[j]-'0')>=(ch[max_index]-'0')){
+                    max_index=j;
+                    count++;
+                }
+            }
+            if(count>0){
+                char temp=ch[i];
+                ch[i]=ch[max_index];
+                ch[max_index]=temp;
+                break;
+            }
+        }
+        return Integer.parseInt(new String(ch));
+    }
+}
