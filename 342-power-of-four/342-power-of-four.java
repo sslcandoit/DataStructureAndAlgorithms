7ddfebbf98db4ca28e25d808c96005f7f9b93010
sslee <91924792+sslcandoit@users.noreply.github.com>
2022-02-04 17:35:30 -0500

    Time: 1 ms (98.31%), Space: 41.3 MB (8.20%) - LeetHub

--- 342-power-of-four/342-power-of-four.java
+++ 342-power-of-four/342-power-of-four.java
@@ -3,7 +3,10 @@ class Solution {
         if(n<=0) return false;
         if(n==1) return true;
         
+        return (n & (n-1))==0 && ((n & 0xaaaaaaaa)==0);
+        /*
         while(n%4==0) n=n/4;
         return n==1;
+        */
     }
 }
