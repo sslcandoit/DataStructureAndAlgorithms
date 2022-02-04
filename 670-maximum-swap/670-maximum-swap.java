048e6c4abee2925aac1b12601fba4355bc453808
sslee <91924792+sslcandoit@users.noreply.github.com>
2022-02-04 18:22:18 -0500

    Time: 1 ms (63.87%), Space: 38.8 MB (12.41%) - LeetHub

--- 670-maximum-swap/670-maximum-swap.java
+++ 670-maximum-swap/670-maximum-swap.java
@@ -6,8 +6,8 @@ class Solution {
             int max_index=i;
             int count=0;
             for(int j=i+1; j<ch.length; j++){
-                if(ch[j]-'0'==ch[i]-'0') continue;
-                if((ch[j]-'0')>=(ch[max_index]-'0')){
+                if(ch[j]-'0'==ch[i]-'0') continue; //in case"98368"
+                if((ch[j]-'0')>=(ch[max_index]-'0')){//in case"1993"
                     max_index=j;
                     count++;
                 }
