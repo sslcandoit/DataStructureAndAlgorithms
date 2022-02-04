c667b56121a94c6628e3222fdaef8d53efa66fce
sslee <91924792+sslcandoit@users.noreply.github.com>
2022-02-04 16:53:19 -0500

    Time: 26 ms (21.01%), Space: 47.3 MB (8.95%) - LeetHub

--- 326-power-of-three/326-power-of-three.java
+++ 326-power-of-three/326-power-of-three.java
@@ -3,6 +3,7 @@ class Solution {
         if(n<=0) return false;
         if(n==1) return true;
         
-        return n%3==0 && isPowerOfThree(n/3);
+        //return n%3==0 && isPowerOfThree(n/3);
+        return 1162261467%n==0;
     }
 }
