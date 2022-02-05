c031bcd52df530936bb73087528da1aec059573b
sslee <91924792+sslcandoit@users.noreply.github.com>
2022-02-05 12:30:23 -0500

    Time: 10 ms (33.78%), Space: 43.3 MB (62.83%) - LeetHub

---
+++ 453-minimum-moves-to-equal-array-elements/453-minimum-moves-to-equal-array-elements.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int minMoves(int[] nums) {
+        Arrays.sort(nums);
+        
+        int ans=0;
+        for(int i=nums.length-1; i>0; i--){
+            ans+=nums[i]-nums[0];
+        }
+        return ans;
+    }
+}
