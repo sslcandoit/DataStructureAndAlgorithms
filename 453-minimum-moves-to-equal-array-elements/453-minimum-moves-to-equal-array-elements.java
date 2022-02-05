6b8a71d7a2af97b6139588bab60d44a4e9a581d2
sslee <91924792+sslcandoit@users.noreply.github.com>
2022-02-05 12:41:38 -0500

    Time: 15 ms (20.35%), Space: 54.3 MB (6.38%) - LeetHub

--- 453-minimum-moves-to-equal-array-elements/453-minimum-moves-to-equal-array-elements.java
+++ 453-minimum-moves-to-equal-array-elements/453-minimum-moves-to-equal-array-elements.java
@@ -1,5 +1,22 @@
 class Solution {
     public int minMoves(int[] nums) {
+        /*
+        minMoves=x, 
+        final all elements become to:m
+        everytime (n-1) to moves
+        
+        sum+x(n-1)=m*n
+        m=min+x;
+        
+        sum+x*n-x=n*min+x*n
+        sum-x=n*min
+        x=sum-n*min;
+        */
+        int sum=Arrays.stream(nums).sum();
+        int min=Arrays.stream(nums).min().getAsInt();
+        int n=nums.length;
+        return sum-min*n;
+        /*
         Arrays.sort(nums);
         
         int ans=0;
@@ -7,5 +24,6 @@ class Solution {
             ans+=nums[i]-nums[0];
         }
         return ans;
+        */
     }
 }
