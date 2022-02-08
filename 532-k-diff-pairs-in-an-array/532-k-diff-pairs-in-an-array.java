37f97081a67f659bc6f77cc95bbaa3bed96e2285
sslee <91924792+sslcandoit@users.noreply.github.com>
2022-02-07 21:34:13 -0500

    Time: 17 ms (39.11%), Space: 49 MB (6.53%) - LeetHub

--- 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.java
+++ 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.java
@@ -1,5 +1,15 @@
 class Solution {
     public int findPairs(int[] nums, int k) {
+        Map<Integer, Integer> map=new HashMap<>();
+        for(int num:nums){
+            map.put(num, map.getOrDefault(num, 0)+1);
+        }
+        int count=0;
+        for(Map.Entry<Integer, Integer> e:map.entrySet()){
+            if(k>0 && map.containsKey(e.getKey()+k) || k==0 && e.getValue()>1) count++;
+        }
+        return count;
+        /*
         Arrays.sort(nums);
         int count=0;
         for(int i=0; i<nums.length-1; i++){
@@ -10,5 +20,6 @@ class Solution {
             }
         }
         return count;
+        */
     }
 }
