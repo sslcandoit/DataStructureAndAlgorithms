d18324c7410951e6ede1ab36fc2df9989dcbd144
sslee <91924792+sslcandoit@users.noreply.github.com>
2022-02-08 12:19:48 -0500

    Time: 2 ms (34.30%), Space: 41.7 MB (7.63%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.java
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.java
@@ -1,5 +1,15 @@
 class Solution {
     public int numIdenticalPairs(int[] nums) {
+        Map<Integer, Integer> map=new HashMap<>();
+        for(int num:nums){
+            map.put(num, map.getOrDefault(num, 0)+1);
+        }
+        int ans=0;
+        for(int e:map.values()){
+            if(e>1) ans+=e*(e-1)/2;
+        }
+        return ans;
+        /*
         int[] count=new int[101];
         int res=0;
         
@@ -7,5 +17,6 @@ class Solution {
             res+=count[num]++;
         }
         return res;
+        */
     }
 }
