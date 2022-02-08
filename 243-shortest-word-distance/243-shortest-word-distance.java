12861021a4070df002919f0129f3345cc99608d7
sslee <91924792+sslcandoit@users.noreply.github.com>
2022-02-08 00:25:01 -0500

    Time: 1 ms (99.75%), Space: 44.5 MB (12.91%) - LeetHub

--- 243-shortest-word-distance/243-shortest-word-distance.java
+++ 243-shortest-word-distance/243-shortest-word-distance.java
@@ -1,5 +1,16 @@
 class Solution {
     public int shortestDistance(String[] wordsDict, String word1, String word2) {
+        int index = -1, minDistance = Integer.MAX_VALUE;
+        for (int i = 0; i < wordsDict.length; i++) {
+            if (wordsDict[i].equals(word1) || wordsDict[i].equals(word2)) {
+                if (index != -1 && !wordsDict[index].equals(wordsDict[i])) {
+                    minDistance = Math.min(minDistance, i - index);
+                }
+                index = i;
+            }
+        }
+   return minDistance;
+        /*
         int index1=-1, index2=-1, min=Integer.MAX_VALUE;
         
         for(int i=0; i<wordsDict.length; i++){
@@ -9,6 +20,7 @@ class Solution {
             
             if(index1 !=-1 && index2 !=-1) min=Math.min(min, Math.abs(index1-index2));
         }
-        return min;   
+        return min;
+        */
     }
 }
