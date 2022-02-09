5f5b55c9296ba7f56a2f0b9d1c1b300b1e4b2e67
sslee <91924792+sslcandoit@users.noreply.github.com>
2022-02-09 00:36:55 -0500

    Time: 155 ms (20.55%), Space: 90.1 MB (5.02%) - LeetHub

--- 912-sort-an-array/912-sort-an-array.java
+++ 912-sort-an-array/912-sort-an-array.java
@@ -1,11 +1,31 @@
 class Solution {
     public int[] sortArray(int[] nums) {
-        shellSort(nums);
+        radixSort(nums);
+        //shellSort(nums);
         return nums;
     }
+    public void radixSort(int[] nums){
+        int mostDigits=(Math.abs(nums[0])+"").length();
+        for(int i=0, x=1; i<mostDigits; i++, x*=10){
+            Map<Integer, List<Integer>> map=new HashMap<>();
+            for(int num:nums){
+                int len=(Math.abs(num)+"").length();
+                if(len>mostDigits) mostDigits=len;
+                
+                int digit=num/x%10;
+                if(!map.containsKey(digit)) map.put(digit, new ArrayList<>());
+                map.get(digit).add(num);
+            }
+            int j=0;
+            for(int index=-9; index<=9; index++){
+                if(map.containsKey(index)){
+                    for(int n:map.get(index)) nums[j++]=n;
+                }
+            }
+        }
+    }
     public void shellSort(int[] nums){
-        int len=nums.length;
-        int h=len/2;
+        int len=nums.length, h=len/2;
         while(h>=1){
             for(int i=h; i<len; i++){
                 for(int j=i-h; j>=0; j-=h){
@@ -17,6 +37,6 @@ class Solution {
                 }
             }
             h=h/2;
-        }//[5,2,3,1,0,9,4,8,6,2,7]
+        }
     }
 }
