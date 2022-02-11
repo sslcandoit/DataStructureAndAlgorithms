bf54f0ba196a95b6dd083817ee1e7729e94d428e
sslee <91924792+sslcandoit@users.noreply.github.com>
2022-02-10 22:41:34 -0500

    Time: 11 ms (42.37%), Space: 42.4 MB (25.39%) - LeetHub

--- 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.java
+++ 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.java
@@ -3,6 +3,11 @@ class Solution {
         Map<Integer, Integer> map=new HashMap<>();
         Arrays.stream(nums).forEach(num-> map.put(num, map.getOrDefault(num, 0)+1));
         
-        return Arrays.stream(nums).boxed().sorted((a,b)->map.get(a)==map.get(b)? b-a:map.get(a)-map.get(b)).mapToInt(n->n).toArray();
+        
+        return Arrays.stream(nums)
+                     .boxed()
+                     .sorted((a,b)->map.get(a)==map.get(b)? b-a:map.get(a)-map.get(b))
+                     .mapToInt(n->n)
+                     .toArray();
     }
 }
