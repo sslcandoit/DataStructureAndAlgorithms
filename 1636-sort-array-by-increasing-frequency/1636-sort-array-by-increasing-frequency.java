23b03883d3842925aec984039eb708557975c2d1
sslee <91924792+sslcandoit@users.noreply.github.com>
2022-02-10 22:23:44 -0500

    Time: 22 ms (6.74%), Space: 44.9 MB (7.18%) - LeetHub

---
+++ 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int[] frequencySort(int[] nums) {
+        Map<Integer, Integer> map=new HashMap<>();
+        Arrays.stream(nums).forEach(num-> map.put(num, map.getOrDefault(num, 0)+1));
+        
+        return Arrays.stream(nums).boxed().sorted((a,b)->map.get(a)==map.get(b)? b-a:map.get(a)-map.get(b)).mapToInt(n->n).toArray();
+    }
+}
