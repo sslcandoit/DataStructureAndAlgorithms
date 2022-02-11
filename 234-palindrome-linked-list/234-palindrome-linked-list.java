65de2c46c705171901e6a8d3bd63ec62e4cc8068
sslee <91924792+sslcandoit@users.noreply.github.com>
2022-02-11 14:00:12 -0500

    Time: 13 ms (34.38%), Space: 99.6 MB (52.34%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.java
+++ 234-palindrome-linked-list/234-palindrome-linked-list.java
@@ -12,9 +12,33 @@ class Solution {
     private ListNode l;
     public boolean isPalindrome(ListNode head) {
         if(head==null || head.next==null) return true;
-        
+        ListNode mid=findMid(head);
+        ListNode head_r=reverse(mid.next);
+        while(head_r !=null){
+            if(head_r.val !=head.val) return false;
+            head_r=head_r.next;
+            head=head.next;
+        }
+        return true;
+        /*
         this.l=head;
         return check(head);
+        */
+    }
+    private ListNode reverse(ListNode head){
+        if(head==null || head.next==null) return head;
+        ListNode n=reverse(head.next);
+        head.next.next=head;
+        head.next=null;
+        return n;
+    }
+    private ListNode findMid(ListNode head){
+        ListNode fast=head, slow=head;
+        while(fast.next !=null && fast.next.next !=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        return slow;
     }
     public boolean check(ListNode head){
         if(head !=null){
@@ -24,4 +48,5 @@ class Solution {
         }
         return true;
     }
+    
 }
