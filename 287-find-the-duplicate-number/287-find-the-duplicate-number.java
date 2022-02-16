3556f531c379af8a7789fdb156e0d51e7a248d89
sslee <91924792+sslcandoit@users.noreply.github.com>
2022-02-16 00:51:16 -0500

    Time: 35 ms (21.92%), Space: 75.2 MB (38.04%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.java
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.java
@@ -1,5 +1,20 @@
 class Solution {
     public int findDuplicate(int[] nums) {
+        //example: [4,6,4,2,1,4,3,5] count(1,2,3,4,5,6,7) = (1,2,3,6,7,8,8). ans=4
+        int l=1;
+        int r=nums.length-1;
+        while(l<r){
+            int m=l+(r-l)/2;
+            int count=0;
+            for(int num:nums){
+                if(num<=m) count++;
+            }
+            if(count>m) r=m;
+            else l=m+1;
+        }
+        return l;
+        
+        /*
         int slow=nums[nums[0]];
         int fast=nums[nums[nums[0]]];
         while(slow !=fast){
@@ -12,5 +27,6 @@ class Solution {
             fast=nums[fast];
         }
         return slow;
+        */
     }
 }
