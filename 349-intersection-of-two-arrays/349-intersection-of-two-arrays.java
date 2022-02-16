95e1f4e769062ae94566829e55dc15f885b89b91
sslee <91924792+sslcandoit@users.noreply.github.com>
2022-02-15 22:04:06 -0500

    Time: 7 ms (21.70%), Space: 43.1 MB (26.11%) - LeetHub

--- 349-intersection-of-two-arrays/349-intersection-of-two-arrays.java
+++ 349-intersection-of-two-arrays/349-intersection-of-two-arrays.java
@@ -1,5 +1,32 @@
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
+        int m1=nums1.length, m2=nums2.length;
+        int[] a=(m1<m2)? nums1:nums2;
+        int[] b=(m2>m1)? nums2:nums1;
+        Arrays.sort(a);
+        Set<Integer> set=new HashSet<>();
+        for(int i=0; i<b.length; i++){
+            if(binarySearch(a, b[i])) set.add(b[i]);
+        }
+        int[] ans=new int[set.size()];
+        int i=0;
+        for(int n:set) ans[i++]=n;
+            
+        return ans;
+    }
+    private boolean binarySearch(int[] nums, int target){
+        int l=0; 
+        int r=nums.length;
+        while(l<r){
+            int m=l+(r-l)/2;
+            if(nums[m]==target) return true;
+            else if(nums[m]>target) r=m;
+            else l=m+1;
+        }
+        return false;
+    }
+}
+/*
         Arrays.sort(nums1);
         Arrays.sort(nums2);
         Set<Integer> set=new HashSet<>();
@@ -17,5 +44,4 @@ class Solution {
         int[] ans=new int[set.size()];
         for(int n:set) ans[i++]=n;
         return ans;
-    }
-}
+        */
