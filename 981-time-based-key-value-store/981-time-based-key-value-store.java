5df04a26ae7837c1a3c20cc4077fc4430568dbe2
sslee <91924792+sslcandoit@users.noreply.github.com>
2022-02-16 10:47:23 -0500

    Time: 257 ms (52.95%), Space: 214.5 MB (62.91%) - LeetHub

--- 981-time-based-key-value-store/981-time-based-key-value-store.java
+++ 981-time-based-key-value-store/981-time-based-key-value-store.java
@@ -1,4 +1,41 @@
 class TimeMap {
+    class Node{
+        int timestamp;
+        String value;
+        public Node(int time, String v){
+            this.timestamp=time;
+            this.value=v;
+        }
+    }
+    Map<String, List<Node>> map;
+    public TimeMap() {
+        this.map=new HashMap<>();
+    }
+    
+    public void set(String key, String value, int timestamp) {
+        if(!map.containsKey(key)) map.put(key, new ArrayList<>());
+        map.get(key).add(new Node(timestamp, value));
+    }
+    
+    public String get(String key, int timestamp) {
+        if(!map.containsKey(key)) return "";
+        List<Node> list=map.get(key);
+        int len=list.size();
+        if(len==1 && list.get(0).timestamp<=timestamp) return list.get(0).value;
+        
+        int l=0;
+        int r=list.size();
+        while(l<r){
+            int m=l+(r-l)/2;
+            if(list.get(m).timestamp>timestamp) r=m;
+            else l=m+1;
+        }
+        if(l==0 && list.get(l).timestamp<=timestamp) return list.get(l).value; 
+        else if(l==0 && list.get(l).timestamp>timestamp) return "";
+        else if(list.get(l-1).timestamp<=timestamp) return list.get(l-1).value;
+        return "";
+    }
+    /*
     Map<String, TreeMap<Integer, String>> map;
     public TimeMap() {
         map=new HashMap<>();
@@ -16,6 +53,7 @@ class TimeMap {
         if(floor==null) return "";
         return temp.get(floor);
     }
+    */
 }
 
 /**
