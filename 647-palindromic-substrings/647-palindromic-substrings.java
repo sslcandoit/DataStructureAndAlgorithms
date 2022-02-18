e603599e4c42a9680d50f9c1051214c0251d8c05
sslee <91924792+sslcandoit@users.noreply.github.com>
2022-02-17 22:50:03 -0500

    Time: 6 ms (69.39%), Space: 41.7 MB (36.70%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.java
+++ 647-palindromic-substrings/647-palindromic-substrings.java
@@ -1,5 +1,14 @@
 class Solution {
     public int countSubstrings(String s) {
+        int len=s.length();
+        int ans=0;
+        for(int i=0; i<len; i++){
+            for(int j=0; i-j>=0 && i+j<len && (s.charAt(i-j)==s.charAt(i+j)); j++) ans++;
+            for(int j=0; i-j>=0 && i+1+j<len && (s.charAt(i-j)==s.charAt(i+1+j)); j++) ans++;
+        }
+        return ans;
+    }
+        /*
         int ans=0;
         for(int i=0; i<s.length(); i++){
             ans+=checkPalindromic(s, i, i);
@@ -17,4 +26,5 @@ class Solution {
         }
         return count;
     }
+    */
 }
