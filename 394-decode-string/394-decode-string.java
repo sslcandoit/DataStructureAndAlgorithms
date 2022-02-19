13a60d293f8d01eda3bd8d52072fff115ab56858
sslee <91924792+sslcandoit@users.noreply.github.com>
2022-02-19 09:52:10 -0500

    Time: 7 ms (17.54%), Space: 43.1 MB (6.16%) - LeetHub

--- 394-decode-string/394-decode-string.java
+++ 394-decode-string/394-decode-string.java
@@ -1,8 +1,33 @@
 class Solution {
+    public String decodeString(String s) {
+        String string="";
+        Stack<Integer> num=new Stack<>();
+        Stack<String> str=new Stack<>();
+        
+        int i=0;
+        while(i<s.length()){
+            if(Character.isDigit(s.charAt(i))){
+                int n=0;
+                while(i<s.length() && Character.isDigit(s.charAt(i))) n=n*10+s.charAt(i++)-'0';
+                num.push(n);
+            }else if(s.charAt(i)=='['){
+                str.push(string);
+                string="";
+                i++;
+            }else if(s.charAt(i)==']'){
+                StringBuilder sb=new StringBuilder(str.pop());
+                int number=num.pop();
+                while(number-- >0) sb.append(string); 
+                string=sb.toString();
+                i++;
+            }else string=string+s.charAt(i++);
+        }
+        return string.toString();
+    }
+    /*
     private int i=0;
     public String decodeString(String s) {
         StringBuilder sb=new StringBuilder();
-        
         while(i<s.length() && s.charAt(i) !=']'){
             if(Character.isDigit(s.charAt(i))){
                 int num=0;
@@ -17,5 +42,5 @@ class Solution {
             }else sb.append(s.charAt(i++));
         }
         return sb.toString();
-    }
+    }*/
 }
