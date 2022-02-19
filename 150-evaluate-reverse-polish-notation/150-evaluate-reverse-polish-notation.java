bd8a442abdd5ba1daa76a0cb9e3f12686398a13f
sslee <91924792+sslcandoit@users.noreply.github.com>
2022-02-19 12:03:36 -0500

    Time: 6 ms (72.95%), Space: 41.4 MB (50.66%) - LeetHub

--- 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.java
+++ 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.java
@@ -1,5 +1,24 @@
 class Solution {
+    private static Map<String, BiFunction<Integer, Integer, Integer>> map=new HashMap<>();
+    static{
+        map.put("+", (a,b)->a+b);
+        map.put("-", (a,b)->a-b);
+        map.put("*", (a,b)->a*b);
+        map.put("/", (a,b)->a/b);
+    }
     public int evalRPN(String[] tokens) {
+        Stack<Integer> stack=new Stack<>();
+        for(String s:tokens){
+            if(map.containsKey(s)){
+                int b=stack.pop();
+                int a=stack.pop();
+                BiFunction<Integer, Integer, Integer> bf=map.get(s);
+                int ans=bf.apply(a,b);
+                stack.push(ans);
+            }else stack.push(Integer.parseInt(s));
+        }
+        return stack.pop();
+        /*
         Stack<Integer> stack=new Stack<>();
         
         for(String s:tokens){
@@ -18,5 +37,6 @@ class Solution {
             }else stack.push(Integer.parseInt(s));
         }
         return stack.pop();
+        */
     }
 }
