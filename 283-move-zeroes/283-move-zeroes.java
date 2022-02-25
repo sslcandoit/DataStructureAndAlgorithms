5453c59469719b585f9d74ed60b5064677c97d66
sslee <91924792+sslcandoit@users.noreply.github.com>
2022-02-24 21:26:58 -0500

    Time: 2 ms (73.38%), Space: 54.7 MB (22.83%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.java
+++ 283-move-zeroes/283-move-zeroes.java
@@ -1,5 +1,13 @@
 class Solution {
     public void moveZeroes(int[] nums) {
+        int count=0, i, j;
+        for(i=0, j=0; j<nums.length; j++){
+            if(nums[j]==0) count++;
+            else nums[i++]=nums[j];
+        }
+        while(count-- >0) nums[i++]=0;
+    }
+        /*
         int len=nums.length;
         int l=0, r=0;
         while(r<len){
@@ -12,4 +20,10 @@ class Solution {
             nums[r]=0;
         }
     }
+    /*
+    [1]
+    [0]
+    [0,1]
+    [1,0]
+    */
 }
