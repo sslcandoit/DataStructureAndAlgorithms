e0b35694feaa29369e0f07cca3ce1bcd4ba057de
sslee <91924792+sslcandoit@users.noreply.github.com>
2022-02-24 22:19:02 -0500

    Time: 18 ms (87.34%), Space: 54.8 MB (5.63%) - LeetHub

--- 277-find-the-celebrity/277-find-the-celebrity.java
+++ 277-find-the-celebrity/277-find-the-celebrity.java
@@ -3,6 +3,15 @@
 
 public class Solution extends Relation {
     public int findCelebrity(int n) {
+        int ans=0;
+        for(int i=1; i<n; i++){
+            if(knows(ans, i)) ans=i;
+        }
+        for(int i=0; i<n; i++){
+            if(ans !=i && (knows(ans, i) ||!knows(i, ans))) return -1;
+        }
+        return ans;
+        /*
         for(int i=0; i<n; i++){
             int j;
             for(j=0; j<n; j++){
@@ -12,5 +21,6 @@ public class Solution extends Relation {
             if(j==n) return i;
         }
         return -1;
+        */
     }
 }
