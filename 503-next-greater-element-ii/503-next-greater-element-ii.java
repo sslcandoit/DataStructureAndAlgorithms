ac1af0991f2ed2cce648aea0bcc9fb75ff5387b7
sslee <91924792+sslcandoit@users.noreply.github.com>
2022-03-01 20:10:40 -0500

    Time: 48 ms (22.14%), Space: 54.6 MB (40.37%) - LeetHub

---
+++ 503-next-greater-element-ii/503-next-greater-element-ii.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] nextGreaterElements(int[] nums) {
+        int n=nums.length;
+        int[] ans=new int[n];
+        Arrays.fill(ans, -1);
+        Stack<Integer> stack=new Stack<>();
+        for(int i=0; i<n*2; i++){
+            while(!stack.isEmpty() && nums[stack.peek()]<nums[i%n]){
+                ans[stack.pop()]=nums[i%n];
+            }
+            stack.push(i%n);
+        }
+        return ans;
+    }
+}
