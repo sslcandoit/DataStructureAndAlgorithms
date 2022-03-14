80010f67a2a3cd3bc8f3498dfcfd72d959cf18a7
sslee <91924792+sslcandoit@users.noreply.github.com>
2022-03-13 21:07:10 -0400

    Time: 6 ms (24.02%), Space: 44.2 MB (44.68%) - LeetHub

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -15,32 +15,22 @@
  */
 class Solution {
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        return helper(0, 0, inorder.length - 1, preorder, inorder);
-    }   
-    public TreeNode helper(int preStart, int inStart, int inEnd, int[] preorder, int[] inorder) {
-        if (preStart > preorder.length - 1 || inStart > inEnd) return null;
-    
-        TreeNode root = new TreeNode(preorder[preStart]);
-        int inIndex = 0; 
-        for (int i = inStart; i <= inEnd; i++) {//找到目前的根的值在中序遍历中是第几个
-            if (inorder[i] == root.val){
-                inIndex = i;
+        TreeNode root=helper(0, 0, inorder.length-1, preorder, inorder);
+        return root;
+    } 
+    public TreeNode helper(int pre_i, int in_start, int in_end, int[] preorder, int[] inorder){
+        if(pre_i>=preorder.length || in_start>in_end) return null;
+        TreeNode root=new TreeNode(preorder[pre_i]);
+        
+        int root_i=0;
+        for(int i=in_start; i<=in_end; i++){
+            if(inorder[i]==root.val){
+                root_i=i;
                 break;
             } 
         }
-        root.left = helper(preStart + 1, inStart, inIndex - 1, preorder, inorder);
-        root.right = helper(preStart + inIndex - inStart + 1, inIndex + 1, inEnd, preorder, inorder);
+        root.left=helper(pre_i+1, in_start, root_i-1, preorder, inorder);
+        root.right=helper(pre_i+(root_i - in_start)+1, root_i+1, in_end, preorder, inorder);
         return root;
     }
-    /*
-Our aim is to find out the index of right child for current node in the preorder array
-    
-We know the index of current node in the preorder array - preStart, it's the root of a subtree
-    
-Remember pre order traversal always visit all the node on left branch before going to the right, therefore, we can get the immediate right child index by skipping all the node on the left branches/subtrees of current node
-    
-The inorder array has this information exactly. Remember when we found the root in "inorder" array we immediately know how many nodes are on the left subtree and how many are on the right subtree
-    
-Therefore the immediate right child index is preStart + numsOnLeft(numsOnLeft = root - inStart) + 1 (remember in preorder traversal array root is always ahead of children nodes but you don't know which one is the left child which one is the right, and this is why we need inorder array)
-    */
 }
