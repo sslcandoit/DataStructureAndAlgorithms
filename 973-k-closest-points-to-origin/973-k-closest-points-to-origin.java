a607393fea033e7a1c737be98cd85bb1a6f2f4e3
sslee <91924792+sslcandoit@users.noreply.github.com>
2022-03-17 17:40:11 -0400

    Time: 116 ms (17.70%), Space: 50.5 MB (89.73%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.java
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.java
@@ -1,22 +1,20 @@
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
-        int m=points.length;
-        quickSelect(points, 0, m-1, k);
+        int l=0, r=points.length-1, pivotIndex=points.length;
+        while(pivotIndex+1 !=k){
+            pivotIndex=partition(points, l, r);
+            if(pivotIndex+1<k) l=pivotIndex+1;
+            else r=pivotIndex;
+        }
         return Arrays.copyOf(points, k);
-    }
-    private void quickSelect(int[][] points, int l, int r, int k){
-        if(l>=r) return;
-        int pivot=partition(points, l, r);
-        if(pivot+1==k) return;
-        else if(pivot+1>k) quickSelect(points, l, pivot, k);
-        else quickSelect(points, pivot+1, r, k);
     }
     private int partition(int[][] points, int l, int r){
         int[] temp=points[l];
+        int pivotDist=calDistance(temp);
         while(l<r){
-            while(l<r && calDistance(points[r])>=calDistance(temp)) r--;
+            while(l<r && calDistance(points[r])>=pivotDist) r--;
             points[l]=points[r];
-            while(l<r && calDistance(points[l])<=calDistance(temp)) l++;
+            while(l<r && calDistance(points[l])<=pivotDist) l++;
             points[r]=points[l];
         }
         points[l]=temp;
