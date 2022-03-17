e4bd8af6ff03003cfabd62e0243c7cf365fdd051
sslee <91924792+sslcandoit@users.noreply.github.com>
2022-03-17 01:57:41 -0400

    Time: 60 ms (16.50%), Space: 54.1 MB (31.42%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.java
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.java
@@ -1,25 +1,20 @@
 class Solution {
     public String minRemoveToMakeValid(String s) {
-        if(s==null || s.length()==0) return "";
+        char[] c=s.toCharArray();
         Stack<Integer> stack=new Stack<>();
-        
-        char[] ch=s.toCharArray();
-        for(int i=0; i<ch.length; i++){
-            if(ch[i]=='(') stack.push(i);
-            else if(ch[i]==')'){
+        for(int i=0; i<s.length(); i++){
+            if(c[i]=='(') stack.push(i);
+            else if(c[i]==')'){
                 if(!stack.isEmpty()) stack.pop();
-                else ch[i]='?';
-            }  
+                else c[i]='*';
+            }
         }
         StringBuilder sb=new StringBuilder();
-        for(int i=ch.length-1; i>=0; i--){
-            
-            if(!stack.isEmpty() && stack.peek()==i){
-                stack.pop();
-                continue;
-            }else if(ch[i]=='?') continue;
-        sb.append(ch[i]);
+        for(int i=c.length-1; i>=0; i--){
+            if(c[i]=='*') continue;
+            if(!stack.isEmpty() && i==stack.peek()) stack.pop();
+            else sb.append(c[i]);
         }
-    return sb.reverse().toString();
+        return sb.reverse().toString();
     }
 }
