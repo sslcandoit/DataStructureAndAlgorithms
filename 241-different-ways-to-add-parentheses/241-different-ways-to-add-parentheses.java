e25e81ebb9405bafd4e6aa3a9abb2fde63a2cc34
sslee <91924792+sslcandoit@users.noreply.github.com>
2022-03-17 21:34:43 -0400

    Time: 1 ms (99.52%), Space: 42.4 MB (58.50%) - LeetHub

---
+++ 241-different-ways-to-add-parentheses/241-different-ways-to-add-parentheses.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<Integer> diffWaysToCompute(String expression) {
+        List<Integer> ans=new ArrayList<>();
+        
+        for(int i=0; i<expression.length(); i++){
+            char c=expression.charAt(i);
+            if(c=='+' || c=='-' || c=='*'){
+                List<Integer> left=diffWaysToCompute(expression.substring(0, i));
+                List<Integer> right=diffWaysToCompute(expression.substring(i+1));
+                for(int l:left){
+                    for(int r:right){
+                        if(c=='+') ans.add(l+r);
+                        else if(c=='-') ans.add(l-r);
+                        else ans.add(l*r);
+                    }
+                }
+            }
+        }
+        if(ans.size()==0) ans.add(Integer.parseInt(expression));
+        return ans;
+    }
+}
