9126ae7d312ed4a28130af0ab69bd05225e91e4d
sslee <91924792+sslcandoit@users.noreply.github.com>
2022-03-27 17:02:29 -0400

    Time: 1 ms (88.73%), Space: 54.6 MB (15.83%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.java
@@ -21,7 +21,7 @@ class Solution {
         }
         for(int j=1; j<n; j++){
             if(matrix[0][j]==0){
-                for(int i=1; i<m; i++) matrix[i][j]=0;
+                for(int i=0; i<m; i++) matrix[i][j]=0;
             }
         }
         if(firstRow){
