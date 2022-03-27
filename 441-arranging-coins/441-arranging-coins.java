3ee76226ba57394aa11b4d3726f6e311d8a96197
sslee <91924792+sslcandoit@users.noreply.github.com>
2022-03-27 14:58:48 -0400

    Time: 2 ms (88.97%), Space: 41.2 MB (69.24%) - LeetHub

--- 441-arranging-coins/441-arranging-coins.java
+++ 441-arranging-coins/441-arranging-coins.java
@@ -1,5 +1,26 @@
 class Solution {
     public int arrangeCoins(int n) {
+  /*      
+long l = 0, r = n, t = 2 * (long) n;
+        while (l < r) {
+            long m = (l + r + 1) / 2;
+            if ((1 + m) * m <= t) l = m;
+            else r = m - 1;
+        }
+        return (int) l;
+        */
+        long l=1, r=n;
+        while(l<r){
+            long m=l+(r-l)/2+1;
+            long sum=(1+m)*m/2;
+   
+            if(sum<=n) l=m;
+            else r=m-1;
+        }
+        return (int)l;    
+    }
+    /* brute force
+    public int arrangeCoins(int n) {
         int row=1;
         while(true){
             n-=row;
@@ -8,4 +29,7 @@ class Solution {
         }
         return row;
     }
+    */
 }
+
+    
