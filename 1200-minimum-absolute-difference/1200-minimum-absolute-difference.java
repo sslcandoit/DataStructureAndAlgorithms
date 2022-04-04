63bbfd4c107876dfb4a606dbcef7ecc2477c5c1d
sslee <91924792+sslcandoit@users.noreply.github.com>
2022-04-04 00:14:03 -0400

    Time: 15 ms (90.31%), Space: 51.8 MB (94.10%) - LeetHub

--- 1200-minimum-absolute-difference/1200-minimum-absolute-difference.java
+++ 1200-minimum-absolute-difference/1200-minimum-absolute-difference.java
@@ -2,18 +2,16 @@ class Solution {
     public List<List<Integer>> minimumAbsDifference(int[] arr) {
         Arrays.sort(arr);
         int minAbs=Integer.MAX_VALUE;
-        for(int i=1; i<arr.length; i++){
-            minAbs=Math.min(minAbs, arr[i]-arr[i-1]);
-        }
-        
         List<List<Integer>> ans=new ArrayList<>();
         for(int i=1; i<arr.length; i++){
-            if(arr[i]-arr[i-1]==minAbs){
-                List<Integer> list=new ArrayList<>();
-                list.add(arr[i-1]);
-                list.add(arr[i]);
-                ans.add(list);
-            } 
+            int cur=arr[i]-arr[i-1];
+            if(cur<minAbs){
+                minAbs=cur;
+                ans.clear();
+                ans.add(Arrays.asList(arr[i-1], arr[i]));
+            }else if(cur==minAbs){
+                ans.add(Arrays.asList(arr[i-1], arr[i]));
+            }
         }
         return ans;
     }
