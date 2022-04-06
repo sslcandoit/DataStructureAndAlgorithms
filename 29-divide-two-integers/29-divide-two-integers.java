72b644ca8a21386b45cecda6f20f8c855920bb1a
sslee <91924792+sslcandoit@users.noreply.github.com>
2022-04-05 23:17:12 -0400

    Time: 2 ms (69.03%), Space: 42.2 MB (6.83%) - LeetHub

--- 29-divide-two-integers/29-divide-two-integers.java
+++ 29-divide-two-integers/29-divide-two-integers.java
@@ -1,26 +1,40 @@
 class Solution {
     public int divide(int dividend, int divisor) {
+        boolean isNegative=false;
+        if((dividend>0 && divisor<0) || (dividend<0 && divisor>0)) isNegative=true;
+        
+        long dividendL=Math.abs((long)dividend), divisorL=Math.abs((long)divisor);
+        if(dividendL<divisorL || dividendL==0 || divisorL==0) return 0;
+        
+        long ans=0;
+        int shift=31;
+        while(dividendL>=divisorL){
+            while(dividendL< (divisorL<<shift)) shift--;
+            dividendL-= (divisorL<<shift);
+            ans+= (1<<shift);
+        }
+        if(ans<0) return isNegative? Integer.MIN_VALUE:Integer.MAX_VALUE;
+        return isNegative? (int)-ans:(int)ans;
+    }/*
+    public int divide(int dividend, int divisor) {
         int sign=1;
         if((dividend>0 && divisor<0) || (dividend<0 && divisor>0)) sign=-1;
         
         long dividendL=Math.abs((long)dividend), divisorL=Math.abs((long)divisor);
-        
         if(dividendL<divisorL || dividendL==0 || divisorL==0) return 0;
         
         long ans=helper(dividendL, divisorL);
         if(ans>Integer.MAX_VALUE) return (sign==1)? Integer.MAX_VALUE:Integer.MIN_VALUE;
         return (int)ans*sign;
-        
     }
     private long helper(long dividend, long divisor){
         if(dividend<divisor) return 0;
-        
-        long multiple=1;
-        long sum=divisor;
+        long multiple=1, sum=divisor;
         while(sum+sum<=dividend){
             sum+=sum;
             multiple+=multiple;
         }
         return multiple+helper(dividend-sum, divisor);
     }
+    */
 }
