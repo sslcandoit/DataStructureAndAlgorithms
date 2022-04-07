4aea7c3565bdaa862ab155242d8429054182cfdc
sslee <91924792+sslcandoit@users.noreply.github.com>
2022-04-07 03:50:46 -0400

    Time: 0 ms (100.00%), Space: 50.1 MB (25.95%) - LeetHub

--- 450-delete-node-in-a-bst/450-delete-node-in-a-bst.java
+++ 450-delete-node-in-a-bst/450-delete-node-in-a-bst.java
@@ -15,6 +15,23 @@
  */
 class Solution {
     public TreeNode deleteNode(TreeNode root, int key) {
+    if(root == null) return null;
+    
+    if(key<root.val) root.left = deleteNode(root.left, key);
+    else if(key>root.val) root.right = deleteNode(root.right, key);
+    else{
+        if(root.left==null) return root.right;
+        else if(root.right==null) return root.left;
+        
+        TreeNode minNode=root.right;
+        while(minNode.left != null) minNode = minNode.left;
+        root.val = minNode.val;
+        root.right = deleteNode(root.right, root.val);
+    }
+    return root;
+}
+    /*
+    public TreeNode deleteNode(TreeNode root, int key) {
         if(root==null) return root;
         
         if(key<root.val) root.left=deleteNode(root.left, key);
@@ -39,5 +56,5 @@ class Solution {
         }
         return root;
     }
-    
+    */
 }
