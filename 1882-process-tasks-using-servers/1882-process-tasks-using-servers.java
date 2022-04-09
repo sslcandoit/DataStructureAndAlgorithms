e0938fe68806b5ca71b626a070d4c0d77c123bc0
sslee <91924792+sslcandoit@users.noreply.github.com>
2022-04-08 20:33:02 -0400

    Time: 591 ms (15.72%), Space: 173.7 MB (72.54%) - LeetHub

--- 1882-process-tasks-using-servers/1882-process-tasks-using-servers.java
+++ 1882-process-tasks-using-servers/1882-process-tasks-using-servers.java
@@ -3,11 +3,13 @@ class Solution {
         PriorityQueue<int[]> freeServers=new PriorityQueue<>((a,b)-> a[0] != b[0]? a[0]-b[0]:a[1]-b[1]);
         for(int i=0; i<servers.length; i++) freeServers.offer(new int[]{servers[i], i, 0});
 
-        PriorityQueue<int[]> processing=new PriorityQueue<>((a,b)-> a[2] != b[2]? a[2]-b[2]: (a[0] !=b[0]? a[0]-b[0] : a[1]-b[1]));
+        PriorityQueue<int[]> processing=
+            new PriorityQueue<>((a,b)-> a[2] != b[2]? a[2]-b[2]: (a[0] !=b[0]? a[0]-b[0] : a[1]-b[1]));
+        
         int[] ans=new int[tasks.length];
         
         for(int i=0; i<tasks.length; i++){
-            while(!processing.isEmpty() && processing.peek()[2]<=i) freeServers.offer(processing.poll());
+            while(!processing.isEmpty() && processing.peek()[2]==i) freeServers.offer(processing.poll());
             
             if(!freeServers.isEmpty()){
                 int[] cur=freeServers.poll();
