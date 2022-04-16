abfc698a81294c66e718ea0c2bf22645cdff117a
sslee <91924792+sslcandoit@users.noreply.github.com>
2022-04-15 21:04:05 -0400

    Time: 9 ms (17.13%), Space: 42.8 MB (68.23%) - LeetHub

--- 833-find-and-replace-in-string/833-find-and-replace-in-string.java
+++ 833-find-and-replace-in-string/833-find-and-replace-in-string.java
@@ -1,6 +1,19 @@
 class Solution {
     public String findReplaceString(String s, int[] indices, String[] sources, String[] targets) {
         int m=s.length();
+         List<int[]> sorted = new ArrayList<>();
+        for (int i = 0 ; i < indices.length; i++) sorted.add(new int[]{indices[i], i});
+        Collections.sort(sorted, Comparator.comparing(i -> -i[0]));
+        for (int[] ind: sorted) {
+            int i = ind[0], j = ind[1];
+            String str = sources[j], t = targets[j];
+            if (i+str.length()<=m && s.substring(i, i + str.length()).equals(str)) s = s.substring(0, i) + t + s.substring(i + str.length());
+        }
+        return s;
+    }
+    /*
+    public String findReplaceString(String s, int[] indices, String[] sources, String[] targets) {
+        int m=s.length();
         int[] match=new int[m];
         Arrays.fill(match, -1);
         
@@ -17,4 +30,5 @@ class Solution {
         }
         return sb.toString();
     }
+    */
 }
