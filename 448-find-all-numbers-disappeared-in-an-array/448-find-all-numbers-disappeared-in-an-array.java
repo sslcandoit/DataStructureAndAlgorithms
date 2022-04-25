66619cfb03141d49f6df7c7f3e510583b3534729
sslee <91924792+sslcandoit@users.noreply.github.com>
2022-04-25 18:53:24 -0400

    Time: 5 ms (91.54%), Space: 67.4 MB (37.88%) - LeetHub

--- 448-find-all-numbers-disappeared-in-an-array/448-find-all-numbers-disappeared-in-an-array.java
+++ 448-find-all-numbers-disappeared-in-an-array/448-find-all-numbers-disappeared-in-an-array.java
@@ -1,5 +1,17 @@
 class Solution {
     public List<Integer> findDisappearedNumbers(int[] nums) {
+        int n=nums.length;
+        for(int i=0; i<n; i++) nums[(nums[i]-1)%n]+=n;
+        
+        List<Integer> ans=new ArrayList<>();
+        for(int i=0; i<n; i++){
+            if(nums[i]<=n) ans.add(i+1);
+        }
+        return ans;
+    }
+    /*
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        //index sorting
         for(int i=0; i<nums.length; i++){
             while(nums[i] !=i+1 && nums[i] !=nums[nums[i]-1]){
                 int temp=nums[i];
@@ -13,4 +25,5 @@ class Solution {
         }
         return ans;
     }
+    */
 }
