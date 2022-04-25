075b027350ab39d9c58a4cc452be1cdd22ff77fc
sslee <91924792+sslcandoit@users.noreply.github.com>
2022-04-25 04:53:37 -0400

    Time: 1 ms (67.21%), Space: 42.4 MB (9.44%) - LeetHub

--- 856-score-of-parentheses/856-score-of-parentheses.java
+++ 856-score-of-parentheses/856-score-of-parentheses.java
@@ -1,4 +1,5 @@
 class Solution {
+    /*
     public int scoreOfParentheses(String s) {
         int ans=0;
         int left=0;
@@ -9,5 +10,18 @@ class Solution {
             if(s.charAt(i)==')' && s.charAt(i-1)=='(') ans+=(1<<left);
         }
         return ans;
+    }
+    */
+    public int scoreOfParentheses(String s) {
+        int score=0;
+        Stack<Integer> stack=new Stack<>();
+        
+        for(int i=0; i<s.length(); i++){
+            if(s.charAt(i)=='('){
+                stack.push(score);
+                score=0;
+            }else score=stack.pop()+Math.max(score*2, 1);
+        }
+        return score;   
     }
 }
