c9221aa3ad17ad7feaf58476abdf49e033dfcb0c
sslee <91924792+sslcandoit@users.noreply.github.com>
2022-04-27 09:09:59 -0400

    Time: 40 ms (37.57%), Space: 96.7 MB (21.05%) - LeetHub

--- 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.java
+++ 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.java
@@ -6,10 +6,9 @@ class Solution {
         
         for(int i=0, j=0; j<nums.length; j++){
             sum+=nums[j];
-            while(sum+k<(long)nums[j]*(j-i+1)){
-                sum-=nums[i];
-                i++;
-            }
+            
+            while(sum+k<(long)nums[j]*(j-i+1)) sum-=nums[i++];
+            
             ans=Math.max(ans, j-i+1);
         }
         return ans;
