4816e28190240d5688bb6a3f2a502934e8a0e72c
sslee <91924792+sslcandoit@users.noreply.github.com>
2022-06-04 19:27:23 -0400

    Time: 10 ms (48.98%), Space: 42.7 MB (66.85%) - LeetHub

--- 299-bulls-and-cows/299-bulls-and-cows.java
+++ 299-bulls-and-cows/299-bulls-and-cows.java
@@ -1,23 +1,19 @@
 class Solution {
     public String getHint(String secret, String guess) {
         int[] count=new int[10];
-        int countA=0, countB=0;
+        int bulls=0, cows=0;
         
         int len=secret.length();
-        for(int i=0; i<len; i++) count[secret.charAt(i)-'0']++;
-        
         for(int i=0; i<len; i++){
-            if(secret.charAt(i)==guess.charAt(i)){
-                countA++;
-                count[guess.charAt(i)-'0']--;
+            char c1=secret.charAt(i);
+            char c2=guess.charAt(i);
+            
+            if(c1==c2) bulls++;
+            else{
+                if(count[c1-'0']++ <0) cows++;
+                if(count[c2-'0']-- >0) cows++;
             }
         }
-        for(int i=0; i<len; i++){
-            if(secret.charAt(i) !=guess.charAt(i) && count[guess.charAt(i)-'0']>0){
-                countB++;
-                count[guess.charAt(i)-'0']--;
-            } 
-        }
-        return countA+"A"+countB+"B";
+        return bulls+"A"+cows+"B";
     }
 }
