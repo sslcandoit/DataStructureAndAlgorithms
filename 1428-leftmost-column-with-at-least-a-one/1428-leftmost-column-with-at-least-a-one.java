a8040cd0f63b3d81efec0eb0b614fe6f3668a540
sslee <91924792+sslcandoit@users.noreply.github.com>
2022-06-03 22:21:37 -0400

    Time: 0 ms (100.00%), Space: 46.9 MB (65.70%) - LeetHub

---
+++ 1428-leftmost-column-with-at-least-a-one/1428-leftmost-column-with-at-least-a-one.java
@@ -0,0 +1,33 @@
+/**
+ * // This is the BinaryMatrix's API interface.
+ * // You should not implement it, or speculate about its implementation
+ * interface BinaryMatrix {
+ *     public int get(int row, int col) {}
+ *     public List<Integer> dimensions {}
+ * };
+ */
+
+class Solution {
+    public int leftMostColumnWithOne(BinaryMatrix binaryMatrix) {
+        List<Integer> list=binaryMatrix.dimensions();
+        int m=list.get(0), n=list.get(1);
+        
+        int left=0, right=n, ans=-1;
+        for(int row=0; row<m; row++){
+            while(left<right){
+                int mid=left+(right-left)/2;
+                if(helper(binaryMatrix, m, mid)){
+                    right=mid;
+                    ans=mid;
+                }else left=mid+1;
+            }
+        }
+        return ans;
+    }
+    private boolean helper(BinaryMatrix bM, int m, int mid){
+        for(int i=0; i<m; i++){
+            if(bM.get(i, mid)==1) return true;
+        }
+        return false;
+    }
+}
