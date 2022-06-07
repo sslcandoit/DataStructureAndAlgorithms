059891cfac553de5a72f6109165b4acc100438f9
sslee <91924792+sslcandoit@users.noreply.github.com>
2022-06-07 00:44:23 -0400

    Time: 23 ms (68.50%), Space: 62.3 MB (72.15%) - LeetHub

--- 1663-smallest-string-with-a-given-numeric-value/1663-smallest-string-with-a-given-numeric-value.java
+++ 1663-smallest-string-with-a-given-numeric-value/1663-smallest-string-with-a-given-numeric-value.java
@@ -1,6 +1,10 @@
 class Solution {
     public String getSmallestString(int n, int k) {
+        int z = (k - n) / 25, r = (k - n) % 25;
+        return (z == n ? "" : "a".repeat(n - z - 1) + (char)('a' + r)) + "z".repeat(z);
+        /*
         int d=(k-n-1)/25, m=(k-n-1)%25;
         return "a".repeat(n-d-1)+(char)('a'+m+1)+"z".repeat(d);
+        */
     }
 }
