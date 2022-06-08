ce68e588aadfb079cb5bd9be81691f950cc2e03f
sslee <91924792+sslcandoit@users.noreply.github.com>
2022-06-07 23:46:19 -0400

    Time: 132 ms (78.00%), Space: 60.8 MB (95.18%) - LeetHub

--- 421-maximum-xor-of-two-numbers-in-an-array/421-maximum-xor-of-two-numbers-in-an-array.java
+++ 421-maximum-xor-of-two-numbers-in-an-array/421-maximum-xor-of-two-numbers-in-an-array.java
@@ -1,5 +1,32 @@
 class Solution {
+    class Node{
+        Node[] child=new Node[2];
+    }
     public int findMaximumXOR(int[] nums) {
+        Node root=new Node();
+        for(int num:nums){
+            Node cur=root;
+            for(int i=31; i>=0; i--){
+                int curBit=(num>>i)&1;
+                if(cur.child[curBit]==null) cur.child[curBit]=new Node();
+                cur=cur.child[curBit];
+            }
+        }
+        int max=0;
+        for(int num:nums){
+            Node cur=root;
+            int curSum=0;
+            for(int i=31; i>=0; i--){
+                int curBit=(num>>i)&1;
+                if(cur.child[curBit^1] !=null) {
+                    curSum+=(1<<i);
+                    cur=cur.child[curBit^1];
+                }else cur=cur.child[curBit];
+            }
+            max=Math.max(max, curSum);
+        }
+        return max;  
+        /*
         int mask=0, ans=0;
         for(int i=31; i>=0; i--){
             mask=mask|(1<<i);
@@ -15,6 +42,6 @@ class Solution {
                 }
             }
         }
-        return ans;
+        return ans;*/
     }
 }
