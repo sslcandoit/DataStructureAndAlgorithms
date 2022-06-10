bf8b9e0a66df05a8fd34d24b8a3cf12bc297c595
sslee <91924792+sslcandoit@users.noreply.github.com>
2022-06-10 01:32:08 -0400

    Time: 8 ms (33.33%), Space: 94.4 MB (20.59%) - LeetHub

--- 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.java
+++ 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.java
@@ -1,6 +1,19 @@
 class Solution {
     public int minDominoRotations(int[] tops, int[] bottoms) {
         int n=tops.length;
+        int[] t=new int[7], b=new int[7], same=new int[7];
+        for(int i=0; i<n; i++){
+            t[tops[i]]++;
+            b[bottoms[i]]++;
+            if(tops[i]==bottoms[i]) same[tops[i]]++;
+        }
+        for(int i=0; i<7; i++){
+            if(t[i]+b[i]-same[i]==n) return n-Math.max(t[i], b[i]);
+        }
+        return -1;
+    }/*
+    public int minDominoRotations(int[] tops, int[] bottoms) {
+        int n=tops.length;
         for(int i=0, t=0, b=0; tops[i]==tops[0] || bottoms[i]==tops[0]; i++){
             if(tops[i] !=tops[0]) t++;
             if(bottoms[i] !=tops[0]) b++;
@@ -12,5 +25,5 @@ class Solution {
             if(i==n-1) return Math.min(t, b);
         }
         return -1;
-    }
+    } */
 }
