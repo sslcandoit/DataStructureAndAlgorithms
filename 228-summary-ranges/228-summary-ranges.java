a9a794da0ca4dc91b0110820d5e0617473890d6a
sslee <91924792+sslcandoit@users.noreply.github.com>
2022-06-10 21:32:10 -0400

    Time: 8 ms (56.65%), Space: 42.5 MB (27.34%) - LeetHub

--- 228-summary-ranges/228-summary-ranges.java
+++ 228-summary-ranges/228-summary-ranges.java
@@ -2,20 +2,17 @@ class Solution {
     public List<String> summaryRanges(int[] nums) {
         List<String> ans=new ArrayList<>();
         int n=nums.length;
-        if(n==0) return ans;
-        
-        int start=nums[0];
-        for(int i=1; i<n; i++){
-            if(nums[i]==nums[i-1]+1) continue;
-            
-            if(nums[i-1]==start) ans.add(""+start);
-            else ans.add(start+"->"+nums[i-1]);
-            
-            start=nums[i];
+        if(n==1){
+            ans.add(""+nums[0]);
+            return ans;
         }
-        if(nums[n-1]==start) ans.add(""+start);
-        else ans.add(start+"->"+nums[n-1]);
-        
+        for(int i=0; i<n; i++){
+            int start=nums[i];
+            while(i+1<n && nums[i+1]==nums[i]+1) i++;
+            
+            if(nums[i]==start) ans.add(""+start);
+            else ans.add(start+"->"+nums[i]);
+        }  
         return ans;
     }
 }
