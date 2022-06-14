ad9d97a6e3ad242aa84db7321de8aea5a53ef585
sslee <91924792+sslcandoit@users.noreply.github.com>
2022-06-14 01:03:53 -0400

    Time: 0 ms (100.00%), Space: 58.4 MB (34.55%) - LeetHub

--- 1060-missing-element-in-sorted-array/1060-missing-element-in-sorted-array.java
+++ 1060-missing-element-in-sorted-array/1060-missing-element-in-sorted-array.java
@@ -1,5 +1,19 @@
 class Solution {
     public int missingElement(int[] nums, int k) {
+        int left = 0, right = nums.length - 1;
+        
+        while (left < right) {
+            int mid = (left + (right - left) / 2) + 1;
+            int missing = nums[mid] - nums[left] - (mid - left);
+            
+            if (missing < k) {
+                k -= missing;
+                left = mid;
+            } else right = mid - 1;
+        }
+        return nums[left] + k;
+        
+        /*
         int n=nums.length, l=0, r=n-1;
         int missed=nums[r]-nums[l]+1-n;
         if(missed<k) return nums[r]+(k-missed);
@@ -15,5 +29,6 @@ class Solution {
             }
         }
         return nums[l]+k;
+        */
     }
 }
