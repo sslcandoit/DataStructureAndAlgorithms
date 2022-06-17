ab71532d35cd1889b260aa937c2a24d3d52ac170
sslee <91924792+sslcandoit@users.noreply.github.com>
2022-06-17 10:54:18 -0400

    Time: 3 ms (26.97%), Space: 45.5 MB (11.80%) - LeetHub

--- 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.java
+++ 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.java
@@ -1,5 +1,6 @@
 class Solution {
-    public int minSubArrayLen(int target, int[] nums) {
+    /*
+    public int minSubArrayLen(int target, int[] nums) {//O(n)
         int l=0, n=nums.length, ans=n+1, sum=0;
         for(int r=0; r<n; r++){
             sum+=nums[r];
@@ -10,5 +11,26 @@ class Solution {
             }
         }
         return ans==n+1? 0:ans;
+    }
+    */
+    public int minSubArrayLen(int target, int[] nums) {
+        int l=1, r=nums.length, ans=0;
+        while(l<=r){
+            int m=l+(r-l)/2;
+            if(helper(nums, target, m)){
+                r=m-1;
+                ans=m;
+            }else l=m+1;
+        }
+        return ans;
+    }
+    private boolean helper(int[] nums, int target, int size){
+        int sum=0;
+        for(int i=0; i<nums.length; i++){
+            if(i>=size) sum-=nums[i-size];
+            sum+=nums[i];
+            if(sum>=target) return true;
+        }
+        return false;
     }
 }
