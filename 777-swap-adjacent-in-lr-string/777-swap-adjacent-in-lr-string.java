d10cf14842079e453a19afa11e37eaec03f7c7d4
sslee <91924792+sslcandoit@users.noreply.github.com>
2022-06-23 01:20:48 -0400

    Time: 5 ms (75.93%), Space: 44.3 MB (48.68%) - LeetHub

--- 777-swap-adjacent-in-lr-string/777-swap-adjacent-in-lr-string.java
+++ 777-swap-adjacent-in-lr-string/777-swap-adjacent-in-lr-string.java
@@ -1,14 +1,14 @@
 class Solution {
     public boolean canTransform(String start, String end) {
-        int n1=start.length(), n2=end.length();
+        int n=start.length();
         int p1=0, p2=0;
         
-        while(p1<n1 || p2<n2){
-            while(p1<n1 && start.charAt(p1)=='X') p1++;
-            while(p2<n2 && end.charAt(p2)=='X') p2++;
+        while(p1<n || p2<n){
+            while(p1<n && start.charAt(p1)=='X') p1++;
+            while(p2<n && end.charAt(p2)=='X') p2++;
             
-            if(p1==n1 && p2==n2) return true;
-            if(p1==n1 || p2==n2) return false;
+            if(p1==n && p2==n) return true;
+            if(p1==n || p2==n) return false;
             
             if(start.charAt(p1) !=end.charAt(p2)) return false;
             
