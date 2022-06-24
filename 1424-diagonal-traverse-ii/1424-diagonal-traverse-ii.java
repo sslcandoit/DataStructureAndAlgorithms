f996577020977916a79c0855f3d40ba8446c8900
sslee <91924792+sslcandoit@users.noreply.github.com>
2022-06-24 11:03:08 -0400

    Time: 59 ms (73.26%), Space: 118.5 MB (63.66%) - LeetHub

--- 1424-diagonal-traverse-ii/1424-diagonal-traverse-ii.java
+++ 1424-diagonal-traverse-ii/1424-diagonal-traverse-ii.java
@@ -1,31 +1,14 @@
 class Solution {
     public int[] findDiagonalOrder(List<List<Integer>> nums) {
-         int len = 0; // count length of result array
-        // reorder list<list<Int>> by Diagonal index(i+j) with reverse order;
-        List<List<Integer>> list = new ArrayList<>(); 
-        for (int i = 0; i < nums.size(); i++){
-            len += nums.get(i).size();
-            for (int j = 0; j < nums.get(i).size(); j++){
-                int idx = i+j;
-                if (idx >= list.size()) list.add(new ArrayList<>());
-                list.get(idx).add(0, nums.get(i).get(j));
-            }
-        } 
-        int[] res = new int[len];
-        int k = 0;
-        for (int i = 0; i < list.size(); i++)
-            for (int j = 0; j < list.get(i).size(); j++)
-                res[k++] = list.get(i).get(j);
-        return res;
-        /*
-        List<List<Integer>> list=new LinkedList<>();
+        
+        List<List<Integer>> list=new ArrayList<>();
         int m=nums.size(), count=0;
         
         for(int i=0; i<m; i++){
             int n=nums.get(i).size();
             count+=n;
             for(int j=0; j<n; j++){
-                if(i+j==list.size()) list.add(new LinkedList<>());
+                if(i+j==list.size()) list.add(new ArrayList<>());
                 list.get(i+j).add(0, nums.get(i).get(j));
             }
         }
@@ -35,6 +18,6 @@ class Solution {
             int j=0;
             while(j<list.get(i).size()) ans[k++]=list.get(i).get(j++);
         }
-        return ans;*/
+        return ans;
     }
 }
