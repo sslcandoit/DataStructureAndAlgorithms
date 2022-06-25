52bdeeb990da8bcdc52c9c580673fe5c974dcaa8
sslee <91924792+sslcandoit@users.noreply.github.com>
2022-06-25 07:14:31 -0400

    Time: 3 ms (37.82%), Space: 42 MB (17.91%) - LeetHub

--- 486-predict-the-winner/486-predict-the-winner.java
+++ 486-predict-the-winner/486-predict-the-winner.java
@@ -1,5 +1,27 @@
 class Solution {
     public boolean PredictTheWinner(int[] nums) {
+        int m=nums.length;
+        int[][] dp=new int[m][m];
+        
+        for(int i=0; i<m; i++) dp[i][i]=nums[i];
+        
+        for(int i=0; i<m-1; i++) dp[i][i+1]=Math.max(nums[i], nums[i+1]);
+        
+        for(int len=3; len<=m; len++){
+            for(int l=0; l+len-1<m; l++){
+                int r=l+len-1;
+                
+                int left=nums[l]+Math.min(dp[l+1][r-1], dp[l+2][r]);
+                int right=nums[r]+Math.min(dp[l+1][r-1], dp[l][r-2]);
+                
+                dp[l][r]=Math.max(left, right);
+            }
+        }
+        int sum=Arrays.stream(nums).sum();
+        return 2*dp[0][m-1]>=sum;
+    }
+    /*
+    public boolean PredictTheWinner(int[] nums) {
         return helper(nums, 0, nums.length-1, new int[nums.length][nums.length])>=0;
     }
     private int helper(int[] nums, int l, int r, int[][] memo){
@@ -10,4 +32,5 @@ class Solution {
         }
         return memo[l][r];
     }
+    */
 }
