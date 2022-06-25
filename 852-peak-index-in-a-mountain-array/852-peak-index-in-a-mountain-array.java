0171c7ff7f03c57ed0751ebabfbb487673b7ec8b
sslee <91924792+sslcandoit@users.noreply.github.com>
2022-06-25 07:54:14 -0400

    Time: 0 ms (100.00%), Space: 46.3 MB (47.72%) - LeetHub

--- 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.java
+++ 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.java
@@ -1,12 +1,17 @@
 class Solution {
     public int peakIndexInMountainArray(int[] arr) {
+        for(int i=0; i+1<arr.length; i++){
+            if(arr[i]>arr[i+1]) return i;
+        }
+        return 0;
+        /*
         int l=0, r=arr.length-1;
         while(l<r){
             int m=l+(r-l)/2;
             if(arr[m]<arr[m+1]) l=m+1;
             else r=m;
         }
-        return l;
+        return l; */
     }
     /* 
              /\
