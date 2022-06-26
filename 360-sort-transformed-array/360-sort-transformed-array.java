79670bc4917a998f19b67a030cdb539251d32ba1
sslee <91924792+sslcandoit@users.noreply.github.com>
2022-06-26 08:25:52 -0400

    Time: 1 ms (89.76%), Space: 43.5 MB (54.87%) - LeetHub

--- 360-sort-transformed-array/360-sort-transformed-array.java
+++ 360-sort-transformed-array/360-sort-transformed-array.java
@@ -1,9 +1,28 @@
 class Solution {
     public int[] sortTransformedArray(int[] nums, int a, int b, int c) {
+        int m=nums.length;
+        int[] ans=new int[m];
+        int l=0, r=m-1, i= (a>=0)? m-1:0;
+        while(l<=r){
+            if(a>=0){
+                ans[i--]=(helper(nums[l], a, b, c)>helper(nums[r], a, b, c))? helper(nums[l++], a, b, c):helper(nums[r--], a, b, c);
+            }else{
+                ans[i++]=(helper(nums[l], a, b, c)<helper(nums[r], a, b, c))? helper(nums[l++], a, b, c):helper(nums[r--], a, b, c);
+            }
+        }
+        return ans;
+    }
+        
+    private int helper(int x, int a, int b, int c){
+        return x*x*a+x*b+c;
+    }
+    /*
+    public int[] sortTransformedArray(int[] nums, int a, int b, int c) {
         for(int i=0; i<nums.length; i++){
             nums[i]=(nums[i]*nums[i]*a)+(nums[i]*b)+c;
         }
         Arrays.sort(nums);
         return nums;
     }
+    */
 }
