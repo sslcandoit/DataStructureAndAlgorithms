29e75d654b0f40382e58e054c4c4d4ff6f182050
sslee <91924792+sslcandoit@users.noreply.github.com>
2022-06-26 11:39:53 -0400

    Time: 3 ms (52.41%), Space: 48.1 MB (55.96%) - LeetHub

--- 338-counting-bits/338-counting-bits.java
+++ 338-counting-bits/338-counting-bits.java
@@ -1,8 +1,12 @@
 class Solution {
     public int[] countBits(int n) {
         int[] ans=new int[n+1];
-        for(int i=1; i<=n; i++){
-            ans[i]=Integer.bitCount(i);
+        
+        for(int i=1, offset=1; i<=n; i++){
+            if(offset*2==i) offset=i;
+            ans[i]=ans[i-offset]+1;
+            //ans[i] = ans[i >> 1] + (i & 1);
+            //ans[i]=Integer.bitCount(i);
         }
         return ans;
     }
