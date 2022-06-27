5418f658a1b1dbd6ba5392a51bad433bf6f84e45
sslee <91924792+sslcandoit@users.noreply.github.com>
2022-06-27 11:19:28 -0400

    Time: 1 ms (93.61%), Space: 44 MB (53.82%) - LeetHub

--- 137-single-number-ii/137-single-number-ii.java
+++ 137-single-number-ii/137-single-number-ii.java
@@ -1,5 +1,13 @@
 class Solution {
     public int singleNumber(int[] nums) {
+        int seenOnce = 0, seenTwice = 0;
+
+        for (int num : nums) {
+             seenOnce = ~seenTwice & (seenOnce ^ num);
+             seenTwice = ~seenOnce & (seenTwice ^ num);
+        }
+        return seenOnce;
+        /*
         Set<Long> set=new HashSet<>();
         long sum=0;
         for(int n:nums){
@@ -8,6 +16,6 @@ class Solution {
         }
         long temp=0;
         for(Long n:set)  temp+=n;
-        return (int)((3*temp-sum)/2);
+        return (int)((3*temp-sum)/2); */
     }
 }
