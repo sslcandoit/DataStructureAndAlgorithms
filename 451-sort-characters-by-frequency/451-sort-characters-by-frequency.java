5a2a7e02d685ee01c28ba6f870e88b6fee126e57
sslee <91924792+sslcandoit@users.noreply.github.com>
2022-06-27 13:14:23 -0400

    Time: 32 ms (36.61%), Space: 54.8 MB (23.86%) - LeetHub

--- 451-sort-characters-by-frequency/451-sort-characters-by-frequency.java
+++ 451-sort-characters-by-frequency/451-sort-characters-by-frequency.java
@@ -7,6 +7,28 @@ class Solution {
             String ss=s.substring(i, i+1);
             map.put(ss, map.getOrDefault(ss, 0)+1);
         }
+        List<String>[] bucket=new List[m+1];
+        for(Map.Entry<String, Integer> e:map.entrySet()){
+            if(bucket[e.getValue()]==null) bucket[e.getValue()]=new ArrayList<>();
+            bucket[e.getValue()].add(e.getKey());
+        }
+        StringBuilder sb=new StringBuilder();
+        for(int i=m; i>0; i--){
+            if(bucket[i]==null) continue;
+            for(String l:bucket[i]){
+                String sub=l.repeat(i);
+                sb.append(sub);
+            }
+        }
+        return sb.toString();
+        /*
+        int m=s.length();
+        
+        Map<String, Integer> map=new HashMap<>();
+        for(int i=0; i<m; i++){
+            String ss=s.substring(i, i+1);
+            map.put(ss, map.getOrDefault(ss, 0)+1);
+        }
         PriorityQueue<Map.Entry<String, Integer>> pq=new PriorityQueue<>((a, b)-> b.getValue()-a.getValue());
         
         for(Map.Entry<String, Integer> e:map.entrySet()) pq.offer(e);
@@ -19,5 +41,6 @@ class Solution {
             sb.append(key);
         }
         return sb.toString();
+        */
     }
 }
