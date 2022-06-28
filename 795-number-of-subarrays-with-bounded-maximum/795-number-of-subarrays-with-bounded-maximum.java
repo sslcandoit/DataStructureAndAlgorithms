473597cfd5df456d8355f977a2bf4f30d225fdaa
sslee <91924792+sslcandoit@users.noreply.github.com>
2022-06-28 12:46:46 -0400

    Time: 5 ms (43.57%), Space: 67 MB (59.04%) - LeetHub

--- 795-number-of-subarrays-with-bounded-maximum/795-number-of-subarrays-with-bounded-maximum.java
+++ 795-number-of-subarrays-with-bounded-maximum/795-number-of-subarrays-with-bounded-maximum.java
@@ -1,5 +1,16 @@
 class Solution {
     public int numSubarrayBoundedMax(int[] nums, int left, int right) {
+            return count(nums, right) - count(nums, left - 1);
+    }
+    private int count(int[] nums, int bound) {
+        int ans = 0, cnt = 0;
+        for (int x : nums) {
+            cnt = x <= bound ? cnt + 1 : 0;
+            ans += cnt;
+        }
+        return ans;
+    }
+        /*
         Stack<Integer> stack=new Stack<>();//递减
         int n=nums.length;
         int ans=0;
@@ -14,5 +25,5 @@ class Solution {
             stack.push(r);
         }
         return ans;
-    }
+        */
 }
