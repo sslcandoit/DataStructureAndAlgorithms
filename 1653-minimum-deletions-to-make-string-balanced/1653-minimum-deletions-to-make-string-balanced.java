643245162d4110e404d598f50bf3044a5263671b
sslee <91924792+sslcandoit@users.noreply.github.com>
2022-06-29 12:09:33 -0400

    Time: 66 ms (29.72%), Space: 42.6 MB (99.40%) - LeetHub

--- 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.java
+++ 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.java
@@ -1,5 +1,17 @@
 class Solution {
     public int minimumDeletions(String s) {
+        int a_right=s.chars().map(c -> c=='a'? 1:0).sum();
+        int m=s.length(), b_left=0;
+        int ans=a_right;
+        for(int i=0; i<m; i++){
+            if(s.charAt(i)=='a') a_right--;
+            else{
+                ans=Math.min(ans, a_right+b_left);
+                b_left++;
+            }
+        }
+        return Math.min(ans, b_left);
+        /*
         int m= s.length();
 	    int[] dp=new int[m+1];
         int bCount=0;
@@ -12,10 +24,10 @@ class Solution {
             }
         }
         return dp[m];
+        */
     }
 }
 /*
 "aaaaa"
 "bbbbb"
-"bbaaa"
 */
