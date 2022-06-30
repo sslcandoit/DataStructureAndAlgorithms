8965c6e9a337af8b6a3cc23afb8a3b2dbce8392b
sslee <91924792+sslcandoit@users.noreply.github.com>
2022-06-30 17:15:53 -0400

    Time: 5 ms (99.93%), Space: 44.9 MB (92.59%) - LeetHub

--- 542-01-matrix/542-01-matrix.java
+++ 542-01-matrix/542-01-matrix.java
@@ -1,5 +1,25 @@
 class Solution {
     public int[][] updateMatrix(int[][] mat) {
+        int m=mat.length, n=mat[0].length, maxDis=m+n;
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(mat[i][j]==0) continue;
+                int left=maxDis, top=maxDis;
+                if(i-1>=0) top=mat[i-1][j];
+                if(j-1>=0) left=mat[i][j-1];
+                mat[i][j]=Math.min(top, left)+1;
+            }
+        }
+        for(int i=m-1; i>=0; i--){
+            for(int j=n-1; j>=0; j--){
+                if(mat[i][j]==0) continue;
+                if(i+1<m) mat[i][j]=Math.min(mat[i][j], mat[i+1][j]+1);
+                if(j+1<n) mat[i][j]=Math.min(mat[i][j], mat[i][j+1]+1);
+            }
+        }
+        return mat;
+            
+        /*
         int m=mat.length, n=mat[0].length;
         Queue<Integer> q=new LinkedList<>();
         
@@ -22,6 +42,6 @@ class Solution {
                 }
             }
         }
-        return mat;
+        return mat;*/
     }
 }
