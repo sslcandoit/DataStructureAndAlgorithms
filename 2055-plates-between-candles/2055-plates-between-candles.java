a87d7f7b61a0a02ca4426b36da802c770ad3e97a
sslee <91924792+sslcandoit@users.noreply.github.com>
2022-07-01 07:24:01 -0400

    Time: 122 ms (23.35%), Space: 141.4 MB (54.01%) - LeetHub

--- 2055-plates-between-candles/2055-plates-between-candles.java
+++ 2055-plates-between-candles/2055-plates-between-candles.java
@@ -1,5 +1,41 @@
 class Solution {
     public int[] platesBetweenCandles(String s, int[][] queries) {
+	    List<Integer> presum = new ArrayList<>(), index = new ArrayList<>();
+	    int n = s.length(), sum = 0;
+	    for (int i = 0; i < n; i++) {
+		    if (s.charAt(i) == '*') sum++;
+		    else {
+			    presum.add(sum);
+			    index.add(i);
+		    }
+	    }
+	    int[] res = new int[queries.length];
+	    for (int i = 0; i < queries.length; i++) {
+		    int start = queries[i][0], end = queries[i][1];
+		    int left = ceiling(index, start), right = floor(index, end);
+		    if (left < right) res[i] = presum.get(right) - presum.get(left);
+	    }
+	    return res;
+    }
+    private int ceiling(List<Integer> list, int target) {
+	    int left = 0, right = list.size();
+	    while (left < right) {
+		    int mid = left + (right - left) / 2;
+		    if (list.get(mid) < target) left = mid + 1;
+		    else right = mid;
+	    }
+	    return left;
+    }
+    private int floor(List<Integer> list, int target) {
+	    int left = -1, right = list.size() - 1;
+	    while (left < right) {
+		    int mid = left + (right - left) / 2 + 1;
+		    if (list.get(mid) > target) right = mid - 1;
+		    else left = mid;
+	    }
+	    return left;
+    }
+        /*                                                                      19ms
         int countCandle[] = new int[s.length() + 1];
         int next[] = new int[s.length() + 1], prev[] = new int[s.length() + 1];
         Arrays.fill(next, Integer.MAX_VALUE);
@@ -15,8 +51,7 @@ class Solution {
             int l = next[queries[j][0]], r = prev[queries[j][1] + 1];
             res[j] = l < r ? r - l - (countCandle[r] - countCandle[l]) : 0;
         }        
-        return res;
-        /*
+        return res;                                                             174ms
         int m=s.length(), n=queries.length, countCandle=0;
         TreeMap<Integer, Integer> map=new TreeMap<>();
         for(int i=0; i<m; i++){
@@ -29,6 +64,5 @@ class Solution {
             ans[i]=r-l-(map.get(r)-map.get(l));
         }
         return ans;                                                                    */
-    }
 }
 
