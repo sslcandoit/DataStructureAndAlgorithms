7013addcec5f7e3cbb1477ab8447856066daf50f
sslee <91924792+sslcandoit@users.noreply.github.com>
2022-07-01 07:42:02 -0400

    Time: 18 ms (23.55%), Space: 46.5 MB (26.79%) - LeetHub

--- 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.java
+++ 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.java
@@ -1,10 +1,13 @@
 class Solution {
     public String removeOccurrences(String s, String part) {
+        return s.contains(part) ? removeOccurrences(s.replaceFirst(part, ""), part) : s;
+        /*
         int n=part.length();
         while(s.indexOf(part) !=-1){
             int i=s.indexOf(part);
             s=s.substring(0, i)+s.substring(i+n);
         }
         return s;
+        */
     }
 }
