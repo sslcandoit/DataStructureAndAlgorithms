d0a4e90cc76a43212d3023db1ceb4fed448844b1
sslee <91924792+sslcandoit@users.noreply.github.com>
2022-09-17 23:05:04 -0400

    Time: 0 ms (100.00%), Space: 41.9 MB (35.73%) - LeetHub

--- 225-implement-stack-using-queues/225-implement-stack-using-queues.java
+++ 225-implement-stack-using-queues/225-implement-stack-using-queues.java
@@ -1,9 +1,33 @@
 class MyStack {
-    Queue<Integer> q1=new LinkedList<>();
-    Queue<Integer> q2=new LinkedList<>();
+    Queue<Integer> q=new LinkedList<>();
 
     public MyStack() {
-        
+    }
+    public void push(int x) {
+        int size=q.size();
+        q.offer(x);
+        for(int i=0; i<size; i++){
+            q.offer(q.poll());
+        }
+    }
+    
+    public int pop() {
+        return q.poll();
+    }
+    
+    public int top() {
+        return q.peek();
+    }
+    
+    public boolean empty() {
+        return q.isEmpty();
+    }
+}
+                                                                  /* Using 2 queues
+class MyStack {
+    Queue<Integer> q1=new LinkedList<>(), q2=new LinkedList<>();
+    
+    public MyStack() {
     }
     
     public void push(int x) {
