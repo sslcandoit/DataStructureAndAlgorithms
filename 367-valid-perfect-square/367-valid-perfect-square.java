f5ab4c01ff517fa48e7986246f3afe3afc5e8641
sslee <91924792+sslcandoit@users.noreply.github.com>
2022-09-18 18:34:06 -0400

    Time: 0 ms (100.00%), Space: 41.2 MB (26.05%) - LeetHub

--- 367-valid-perfect-square/367-valid-perfect-square.java
+++ 367-valid-perfect-square/367-valid-perfect-square.java
@@ -1,11 +1,19 @@
 class Solution {
     public boolean isPerfectSquare(int num) {
+        long x=num;
+        
+        while(x*x>num){
+            x=(num/x+x)>>1;
+        }
+        return x*x==num;
+        
+        /*
         int target=(num/2)+1;
         
         while(target>=1){
             if(target*target==num) return true;
             target--;
         }
-        return false;
+        return false;*/
     }
 }
