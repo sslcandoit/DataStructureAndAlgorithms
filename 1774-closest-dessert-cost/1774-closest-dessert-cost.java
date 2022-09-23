1baa5b1a3615e4dbf8310963638582487a868d65
sslee <91924792+sslcandoit@users.noreply.github.com>
2022-09-22 23:37:29 -0400

    Time: 30 ms (26.65%), Space: 41.3 MB (69.54%) - LeetHub

---
+++ 1774-closest-dessert-cost/1774-closest-dessert-cost.java
@@ -0,0 +1,26 @@
+class Solution {
+    Integer ans=null;
+    public int closestCost(int[] baseCosts, int[] toppingCosts, int target) {
+        for(int i=0; i<baseCosts.length; i++){
+            helper(toppingCosts, target, 0, baseCosts[i]);
+        }
+        return ans;
+    }
+    public void helper(int[] topping, int target, int i, int cur){
+        if(i==topping.length){
+            if(this.ans==null 
+               || Math.abs(cur-target)<Math.abs(this.ans-target) 
+               || (Math.abs(cur-target)==Math.abs(this.ans-target) && cur<this.ans) ) ans=cur;
+            return;
+        }
+        
+        cur+= topping[i]*2;
+        helper(topping, target, i+1, cur);
+        
+        cur-= topping[i];
+        helper(topping, target, i+1, cur);
+        
+        cur-= topping[i];
+        helper(topping, target, i+1, cur);
+    }
+}
